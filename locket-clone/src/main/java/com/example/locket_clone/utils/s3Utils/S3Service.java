ce1cd52ffcac01d5f29086be4808f3a0a60159a8
phamsongtoan1910 <85654638+phamsongtoan1910@users.noreply.github.com>
2025-04-04 23:12:12 +0700

    use s3 to upload image

---
+++ locket-clone/src/main/java/com/example/locket_clone/utils/s3Utils/S3Service.java
@@ -0,0 +1,37 @@
+package com.example.locket_clone.utils.s3Utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucketName}")
+    private String bucketName;
+
+    private String cloudFrontDomain = System.getenv("AWS_CLOUD_FRONT_DOMAIN");
+
+    public String uploadFile(MultipartFile file) throws IOException {
+        String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename();
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
+
+        return cloudFrontDomain + fileName;
+    }
+}
